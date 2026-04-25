6dbc25a91012c5212e61cdaea10d09890d10d9f8
FairSky <xyboy52158@gmail.com>
2026-04-26 03:01:56 +0800

    final CLI order system v2 + README update

---
+++ cli_order_v2.py
@@ -0,0 +1,76 @@
+menu = {
+    "咖啡": 50,
+    "蛋糕": 80,
+    "紅茶": 30
+}
+
+total = 0
+order_dict = {}
+
+# 輸入
+while True:
+    ordering = input("請輸入指令: ")
+
+
+    if ordering == "結束":
+        break
+
+
+    parts = ordering.split()
+    command = parts[0]
+
+
+    # 刪除
+    if command == "刪除":
+        name = parts[1]
+
+        if name in order_dict:
+            del order_dict[name]
+        else:
+            print("查無此商品")
+    
+
+    # 更改
+    elif command == "更改":
+        name = parts[1]
+        count = int(parts[2])
+
+        if name in order_dict:
+            order_dict[name] = count
+        else:
+            print("查無此商品")
+
+
+    # 累加、新增
+    else:
+        name = parts[0]
+        count = int(parts[1])
+        if name not in menu:
+            print("查無此商品")
+            continue
+
+        elif name in order_dict:
+            order_dict[name] += count
+            
+        else:
+            order_dict[name] = count
+
+
+# 明細、計算金額
+for name , count in order_dict.items():
+
+    subtotal = menu[name] * count
+    print(f"{name} x {count} = {subtotal}")
+
+    total += subtotal
+
+print(f"總金額是: {total}")
+
+    
+
+
+
+
+
+
+
